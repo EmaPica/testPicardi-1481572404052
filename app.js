d6f7524995855d902069a467feac01e8124fe35a
EmaPica <emanuela.picardi-cic-it@it.ibm.com>
2016-12-12 21:12:15 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6,26 +6,40 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
-'use strict';
+var express = require('express');
 
-var express = require('express'),
-    tradeoffAnalyticsConfig = require('./tradeoff-analytics-config');
+// cfenv provides access to your Cloud Foundry environment
+// for more info, see: https://www.npmjs.com/package/cfenv
+var cfenv = require('cfenv');
 
+// create a new express server
 var app = express();
-app.use('/',express.static(__dirname + '/public'));
 
-// For local development, copy your service instance credentials here, otherwise you may ommit this parameter
-var serviceCredentials = {
-  username: 'da90de8a-b9d3-49d1-9878-bb5b3e1972bf',
-  password: 'TKci3od4UVUD'
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
+// get the app environment from Cloud Foundry
+var appEnv = cfenv.getAppEnv();
+
+System-Provided:
+{
+"VCAP_SERVICES": {
+  "tradeoff_analytics": [{
+      "credentials": {
+        "url": "https://gateway.watsonplatform.net/tradeoff-analytics/api",
+        "password": "TKci3od4UVUD",
+        "username": "da90de8a-b9d3-49d1-9878-bb5b3e1972bf"
+      },
+    "label": "tradeoff-analytics",
+    "name": "tradeoff-analytics-standard-service",
+    "plan": "standard"
+ }]
 }
-// When running on Bluemix, serviceCredentials will be overriden by the credentials obtained from VCAP_SERVICES
-tradeoffAnalyticsConfig.setupToken(app, serviceCredentials); 
+}
+// start server on the specified port and binding host
+app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+  console.log("server starting on " + appEnv.url);
+});
 
-// to communicate with the service using a proxy rather then a token, add a dependency on "body-parser": "^1.15.0" 
-// to package.json, and use:
-// tradeoffAnalyticsConfig.setupProxy(app, serviceCredentials);
 
-var port = process.env.VCAP_APP_PORT || 2000;
-app.listen(port);
-console.log('listening at:', port);
